a79ea6b8ca7c6b443e606d38d5a219dfb0d64214
Akshay Apte <akshayapte7@gmail.com>
2022-12-21 00:27:49 +0530

    add solution for day 13 part 1

---
+++ 13.py
@@ -0,0 +1,49 @@
+import json
+
+
+def in_order(p1, p2):
+	for i in range(min(len(p1), len(p2))):
+		
+		# int int
+		if type(p1[i]) == int and type(p2[i]) == int:
+			if p1[i]==p2[i]:
+				continue
+			return p1[i]<p2[i]
+
+		# one int one list
+		if type(p1[i]) == int or type(p2[i]) == int:
+			if type(p1[i]) == int:
+				ret = in_order([p1[i]], p2[i])
+			elif type(p2[i]) == int:
+				ret = in_order(p1[i], [p2[i]])
+			if ret is None:
+				continue
+			return ret
+
+		# both lists
+		if type(p1[i]) == list and type(p2[i]) == list:
+			ret = in_order(p1[i], p2[i])
+			if ret is None:
+				continue
+			return ret
+
+	if len(p1) != len(p2):
+		return len(p1) < len(p2)
+
+
+def part_one():
+	with open("13.txt", "r") as inp:
+		packet_pairs = inp.read().split("\n\n")
+		ans = 0
+		for i, packet_pair in enumerate(packet_pairs):
+			packet_pair = packet_pair.split("\n")
+			packet1 = json.loads(packet_pair[0].strip())
+			packet2 = json.loads(packet_pair[1].strip())
+			if in_order(packet1, packet2):
+				ans += i+1
+
+	print(ans)
+
+
+if __name__ == "__main__":
+	part_one()
